1442bd64e625787df51a37a42b041e2d3510494a
oskarro <oskar.slyk@gmail.com>
2020-02-08 15:19:30 +0100

    init proj

---
+++ src/main/java/com/oskarro/codecity/CodecityApplication.java
@@ -0,0 +1,13 @@
+package com.oskarro.codecity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodecityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oskarro/codecity/CodecityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oskarro.codecity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodecityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
