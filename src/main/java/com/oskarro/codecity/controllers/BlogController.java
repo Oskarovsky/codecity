18a4740d03acbe4e4df4f6a419ba8a318d81dcb5
oskarro <oskar.slyk@gmail.com>
2020-02-09 17:22:59 +0100

    db support

--- src/main/java/com/oskarro/codecity/controllers/BlogController.java
+++ src/main/java/com/oskarro/codecity/controllers/BlogController.java
@@ -1,13 +1,39 @@
 package com.oskarro.codecity.controllers;
 
+import com.oskarro.codecity.entities.Post;
+import com.oskarro.codecity.service.PostService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
 
 @RestController
 public class BlogController {
 
+    private PostService postService;
+
+    public BlogController(PostService postService) {
+        this.postService = postService;
+    }
+
     @GetMapping(value = "/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping(value = "/posts")
+    public List<Post> posts() {
+        return postService.getAllPosts();
+    }
+
+    @PostMapping(value = "/post")
+    public void publishPost(@RequestBody Post post) {
+        if(post.getDateCreated() == null) {
+            post.setDateCreated(new Date());
+        }
+        postService.addPost(post);
     }
 }
--- src/main/java/com/oskarro/codecity/service/PostService.java
+++ src/main/java/com/oskarro/codecity/service/PostService.java
@@ -1,7 +1,10 @@
 package com.oskarro.codecity.service;
 
+import com.oskarro.codecity.entities.Post;
 import com.oskarro.codecity.repositories.PostRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PostService {
@@ -10,5 +13,13 @@ public class PostService {
 
     public PostService(PostRepository postRepository) {
         this.postRepository = postRepository;
+    }
+
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+
+    public void addPost(Post post) {
+        postRepository.save(post);
     }
 }
